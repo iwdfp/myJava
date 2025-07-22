6ee1f0b553c415e8f4b299e525e49ae9ca79c529
Danil <danilalexeev162@gmail.com>
2025-07-22 18:02:58 +0500

    added homework_1

---
+++ src/homework_1/Homework_1_5.java
@@ -0,0 +1,14 @@
+package homework_1;
+
+public class Homework_1_5 {
+
+    public static void main(String[] args) {
+
+        int q = 78;
+        int w = 5;
+
+        System.out.println(q/w);
+        System.out.println(q%w);
+
+    }
+}
---
+++ src/homework_1/practice.java
@@ -0,0 +1,12 @@
+package homework_1;
+
+public class practice {
+
+    public static void main(String[] args) {
+
+        int age = 24;
+
+        System.out.println("Алексеев Данил и мой возраст: " + age);
+
+    }
+}
